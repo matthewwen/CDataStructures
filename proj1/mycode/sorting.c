f361107b30caa536b9934aa4c7d89ea4d1f2add8
Matthew Wen <mattwen2018@gmail.com>
2019-09-15 05:46:32 -0400

    /2019-09-15T05:46:32-0400

--- proj1/mycode/sorting.c
+++ proj1/mycode/sorting.c
@@ -15,10 +15,12 @@ long *Load_File(char * Filename, int * Size) {
 
     long * num = malloc(sizeof(*num) * *Size);
 
-    for (int i = *Size - 1; i >= 0; i--) {
+	int i;
+    for (i = *Size - 1; i >= 0; i--) {
         num[i] = 0;
         fseek(fp, -1L, SEEK_CUR);
-        for (int j = 1; num_elements > 0 && (c = fgetc(fp)) != '\n'; j *= 10){
+		int j;
+        for (j = 1; num_elements > 0 && (c = fgetc(fp)) != '\n'; j *= 10){
             num[i] += j * (c - '0');
             num_elements -= 1;
             fseek(fp, -2L, SEEK_CUR);
@@ -33,9 +35,11 @@ long *Load_File(char * Filename, int * Size) {
 int Save_File(char *Filename, long *Array, int Size) {
     FILE * fp = fopen(Filename, "w");
     int size  = 2;
-    for(long val = LONG_MAX; val > 0 && (++size > 0); val /= 10);
+	long val;
+    for(val = LONG_MAX; val > 0 && (++size > 0); val /= 10);
     char * buffer = malloc(size * sizeof(*buffer));
-    for (int i = 0; i < Size; i++) {
+	int i;
+    for (i = 0; i < Size; i++) {
         sprintf(buffer, "%ld\n", Array[i]);
         fwrite(buffer, sizeof(*buffer), strlen(buffer), fp);
     }
@@ -51,10 +55,12 @@ void Improved_Bubble_Sort(long *Array, int Size, double *N_Comp, double *N_Move)
     int left_idx  = 0;
     int right_idx = Size - 1;
 
-    for (int i = 0; i < Size && (left_idx >= 0) && (right_idx >= 0); i++) {
+	int i;
+    for (i = 0; i < Size && (left_idx >= 0) && (right_idx >= 0); i++) {
         bool go_left = (i % 2) == 0;
         int pos      = -1;
-        for (int j = go_left ? left_idx : right_idx; j <= right_idx && j >= left_idx; j += go_left ? 1: -1) {
+		int j;
+        for (j = go_left ? left_idx : right_idx; j <= right_idx && j >= left_idx; j += go_left ? 1: -1) {
             *N_Comp = *N_Comp + 1;
             if (Array[j] > Array[j + 1]) {
                 Array[j]     = Array[j] + Array[j + 1];
