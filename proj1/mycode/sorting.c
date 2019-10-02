7ada790704f72c798cc32ba184c26cff400a8047
Matthew Wen <mattwen2018@gmail.com>
2019-10-01 23:47:55 -0400

    /2019-10-01T23:47:55-0400

--- proj1/mycode/sorting.c
+++ proj1/mycode/sorting.c
@@ -121,7 +121,6 @@ void Shell_Insertion_Sort(long *Array, int Size, double *N_Comp, double *N_Move)
 void Improved_Bubble_Sort(long *Array, int Size, double *N_Comp, double *N_Move) {
 	double local_comp = 0.0;
 	double local_move = 0.0;
-	sequence_t info = One_N_Seq(Size);
 	int j, k;
 	long temp, temp_mgap;
 	int gap = Size;
@@ -131,62 +130,80 @@ void Improved_Bubble_Sort(long *Array, int Size, double *N_Comp, double *N_Move)
 		}
 		for (j = gap; j < Size; j++) {
 			for (k = j; k >= gap && (temp_mgap = Array[k - gap]) > (temp = Array[k]); k -= gap) {
-				local_move = local_move + 1;
+				local_move = local_move + 3;
+				local_comp = local_comp + 1;
 				Array[k]       = temp_mgap;
 				Array[k - gap] = temp; 
 			}
+			local_comp = local_comp + 1;
 		}
-		local_comp = local_comp + Size - gap + 1;
 		gap = gap / 1.3;
 	}
-	local_comp = local_comp + local_move;
 	*N_Comp += local_comp;
 	*N_Move += local_move;
-	free(info.seq);
 }
 
 sequence_t Two_Three_Seq(int Size) {
-	int * seq = malloc(Size * sizeof(*seq));
-	seq[0] = 1;
-	int p2 = 0;
-	int p3 = 0;
-	int s  = 0;
+	int degree = 0;
+	
+	int i, j;
+	for(i = 1; i * 3 < Size && ++degree; i *= 3) {}
+	int * seq_carrier = malloc((degree + 1) * sizeof(*seq_carrier));
 
-	int i;
-	int u2 = 0;
-	int u3 = 0;
-	for (i = 1; u2 < Size && u3 < Size && i < Size; i++){
-		if (seq[p2] * 2 == seq[i - 1]) {
-			p2 += 1;
+	int two_mult = 1;
+	int num_seq  = degree + 1;
+	int num_two  = 0;
+	for (j = degree; i > 0; i /= 3) {
+		seq_carrier[j] = i * two_mult; 
+		for (;seq_carrier[j] * 2 < Size; seq_carrier[j] *= 2) {
+			two_mult *= 2;
+			num_two++;
 		}
-		if (seq[p3] * 3 == seq[i -1]) {
-			p3 += 1;
-		}
-		u2 = seq[p2] * 2;
-		u3 = seq[p3] * 3;
-		if (u2 < u3) {
-			p2 += 1;
-			seq[i] = u2;
-		}
-		else {
-			p3 += 1;
-			seq[i] = u3;
-		}
-		s++;
+		num_seq += num_two;
+		j--;
 	}
 
-	return (sequence_t) {.seq = seq, .size = s};
+	int * seq = malloc(num_seq * sizeof(*seq));
+	int last_idx = degree;
+	for (i = 0; i < num_seq; i++) {
+		int max_idx = 0;
+		long max_value = seq_carrier[max_idx];	
+		for (j = 1; j <= last_idx; j++) {
+			long temp;
+			if (seq_carrier[j] != -1 && max_idx < (temp = seq_carrier[j])) {
+				max_idx = j;
+				max_value = temp;
+			}
+		}
+		seq[i] = max_value;
+		if (seq_carrier[max_idx] % 2 != 0) {
+			seq_carrier[max_idx] = seq_carrier[last_idx];
+			last_idx--;
+		}
+		else {
+			seq_carrier[max_idx] /= 2;
+		}
+	}
+	free(seq_carrier);
+
+	return (sequence_t) {.seq = seq, .size = num_seq};
 
 }
 
 sequence_t One_N_Seq(int Size) {
-	int * seq = malloc(sizeof(*seq) * Size);
+	int increment = Size / 10;
+	int num_alloc = Size / 10;
+	int * seq = malloc(sizeof(*seq) * num_alloc);
 	int N = Size;
 	int i;
 	int s = 0;
 	for (i = 0; N > 1 && i < Size; i++) {
 		N /= 1.3;
 		N = (N == 9) || (N == 10) ? 11: N;
+		if (i >= num_alloc) {
+			num_alloc += increment;
+			seq = realloc(seq, num_alloc * sizeof(*seq));
+		}
 		seq[i] = N;
 		s++;
 	}
@@ -195,20 +212,24 @@ sequence_t One_N_Seq(int Size) {
 
 void Save_Seq1(char * Filename, int N) {
 	sequence_t info = Two_Three_Seq(N);
-	save_file(Filename, info);
+	FILE * fp = fopen(Filename, "w");
+	int i;
+	fprintf(fp, "%d\n", info.size);
+	for (i = info.size - 1; i >= 0; i--) {
+		fprintf(fp, "%d\n", info.seq[i]);
+	}
+	free(info.seq);
+	fclose(fp);
 }
 
 void Save_Seq2(char * Filename, int N) {
-	//sequence_t info = FCK_Two_Three_Seq(N);
 	sequence_t info = One_N_Seq(N);
-	save_file(Filename, info);
-}
-
-void save_file(char * Filename, sequence_t info) {
 	FILE * fp = fopen(Filename, "w");
 	int i;
+	fprintf(fp, "%d\n", info.size);
 	for (i = 0; i < info.size; i++) {
 		fprintf(fp, "%d\n", info.seq[i]);
 	}
+	free(info.seq);
 	fclose(fp);
 }
