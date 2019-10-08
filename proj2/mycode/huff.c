65bb5c65c10a8d3adc855b27e5ee7e473ef3dedc
Matthew Wen <mattwen2018@gmail.com>
2019-10-08 03:37:33 -0500

    /2019-10-08T03:37:33-05:00

--- proj2/mycode/huff.c
+++ proj2/mycode/huff.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "huff.h"
 
 int main(int argc, char* argv[]) {
-	
-	return EXIT_SUCCESS;
+	bool input_valid = false;
+		
+	for (int i = 0; i < argc; i++) {
+		printf("%s\n", argv[i]);
+	}
+	if ((input_valid = (argc == 2))) {
+		printf("valid input");
+	}
+	return input_valid ? EXIT_SUCCESS: EXIT_FAILURE;
 }
 /* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
--- proj2/mycode/unhuff.c
+++ proj2/mycode/unhuff.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "huff.h"
 
 int main(int argc, char* argv[]) {
-	
-	return EXIT_SUCCESS;
+	bool input_valid = false;
+		
+	if ((input_valid = (argc == 2))) {
+		printf("valid input");
+	}
+	return input_valid ? EXIT_SUCCESS: EXIT_FAILURE;
 }
 /* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
