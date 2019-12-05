13a0451d6a7d7dfdcc8c1a9eecb65454e397e14e
Matthew Wen <mattwen2018@gmail.com>
2019-12-04 22:10:44 -0500

    /2019-12-04T22:10:44-05:00

---
+++ proj3/mycode/header.h
@@ -0,0 +1,44 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <inttypes.h>
+
+struct llong_t{
+    int idx;
+    struct llong_t * next;
+};
+typedef struct llong_t llong_t;
+
+typedef struct{
+    struct {
+        int x;
+        int y;
+    } coord;
+    llong_t * adj_head; // Linked list
+    int minidx; // idx at the min heap
+    uint16_t distance; // dijkstra algorithm
+}Node_t;
+
+typedef struct{
+    void * heap;
+    long size;
+    long idx;
+}ListNode;
+
+typedef struct{
+    int curr_idx;
+    int curr_pidx;
+    Node_t * nodes;
+    int * prev;
+    ListNode * heap;
+}dijkstra_t;
+
+bool read_cord(char * file_name, ListNode * a_node);
+bool dijkstra(int node1, int node2, ListNode list_node,  ListNode * list_heap, int ** a_prev);
+void free_nodes(Node_t * nodes, int size);
+
+#endif
+/* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
